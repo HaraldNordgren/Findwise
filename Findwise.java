401cae94bab4f519ba9ee572a98db8523f893276
HaraldNordgren <haraldnordgren@gmail.com>
2017-01-11 00:43:13 +0100

    Working multi-word tf-idf

--- Findwise.java
+++ Findwise.java
@@ -1,20 +1,35 @@
 import java.util.*;
+import java.lang.Math.*;
 
 
 public class Findwise {
+
+    static <K,V extends Comparable<? super V>>
+    SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
+        SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
+            new Comparator<Map.Entry<K,V>>() {
+                @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
+                    int res = e2.getValue().compareTo(e1.getValue());
+                    return res != 0 ? res : 1;
+                }
+            }
+        );
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
+
     public static void main(String[] args) {
         
-        Map<String, String> documents = new HashMap<String, String>();
+        Map<String, String> documents = new HashMap<>();
         documents.put("document 1", "the brown fox jumped over the brown dog");
         documents.put("document 2", "the lazy brown dog sat in the corner");
         documents.put("document 3", "the red fox bit the lazy dog");
+        //documents.put("document 4", "brown brown brown");
 
-        if (args.length < 1) {
+        if (args.length == 0) {
             System.err.println("Please provide a search query!");
             System.exit(1);
         }
-
-        String query = args[0];
 
         HashMap<String, TreeMap<String, Integer>> reversed_index = new HashMap<>();
 
@@ -48,14 +63,33 @@ public class Findwise {
             }
         }
 
-        TreeMap<String, Integer> document_occurences = reversed_index.get(query);
-        Set<String> result;
+        double total_documents = documents.size();
 
-        if (document_occurences == null) {
-            System.out.println("<No results>");
-        } else {
-            result = document_occurences.keySet();
-            System.out.println(result);
+        TreeMap<String, Double> total_tf_ids = new TreeMap<>();
+        /*for (String key : documents.keySet()) {
+            total_tf_ids.put(key, 0.);
+        }*/
+
+        for (String query_string : args) {
+            //System.out.println(query_string);
+
+            TreeMap<String, Integer> document_occurences = reversed_index.get(query_string);
+            if (document_occurences == null) {
+                continue;
+            }
+            
+            int documents_containing_word = document_occurences.size();
+            double idf = java.lang.Math.log(total_documents / documents_containing_word);
+
+            for (String document : document_occurences.keySet()) {
+                int word_occurences = document_occurences.get(document);
+                Double tf_id = total_tf_ids.get(document);
+                double current_tf_id = (tf_id != null) ? tf_id : 0.;
+                total_tf_ids.put(document, current_tf_id + word_occurences * idf);
+            }
         }
+
+
+        System.out.println(entriesSortedByValues(total_tf_ids));
    }
 }
